6562ca44a30660098595502f5295af7fa327a614
tal_l <tal9lev@gmail.com>
2024-07-25 03:00:45 +0300

    style: :lipstick: better status code of Response

--- todo_app_api/api/views.py
+++ todo_app_api/api/views.py
@@ -1,10 +1,12 @@
 # Create your views here.
+from django.shortcuts import get_object_or_404
 from django.test import TestCase
-from .serializers import GroupSerializer, TaskSerializer, permissionserializer
-from .models import Group, Task, Permissions
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from django.shortcuts import get_object_or_404
+from rest_framework import status
+from .models import Group, Task, Permissions
+from .serializers import GroupSerializer, TaskSerializer, Permissionserializer
+
 
 @api_view(['GET','POST'])
 def groups(request):
@@ -17,12 +19,12 @@ def groups(request):
         serializer=GroupSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
-            return Response(serializer.errors)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
    
 @api_view(['GET','PUT','DELETE'])
-def groups_id(request, id):
+def group(request, id):
     if request.method=='GET':
         group=get_object_or_404(Group, id=id)
         serializer=GroupSerializer(group)
@@ -35,6 +37,66 @@ def groups_id(request, id):
     if request.method=='PUT':
         group=get_object_or_404(Group, id=id)
         serializer=GroupSerializer(group, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
+
+@api_view(['GET', 'POST', 'PUT', 'DELETE'])
+def tasks(request, id):
+    if request.method == 'GET':
+        task = get_object_or_404(Task, id=id)
+        serializer = TaskSerializer(task)
+        return Response(serializer.data)
+
+    if request.method == 'DELETE':
+        task = get_object_or_404(Task, id=id)
+        task.delete()
+        return Response(status=204)
+
+    if request.method == 'PUT':
+        task = get_object_or_404(Task, id=id)
+        serializer = TaskSerializer(task, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
+    if request.method == 'POST':
+        serializer = TaskSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
+
+@api_view(['GET', 'POST', 'PUT', 'DELETE'])
+def permissions(request, id):
+    if request.method == 'GET':
+        permission = get_object_or_404(Permissions, id=id)
+        serializer = Permissionserializer(permission)
+        return Response(serializer.data)
+
+    if request.method == 'DELETE':
+        permission = get_object_or_404(Permissions, id=id)
+        permission.delete()
+        return Response(status=204)
+
+    if request.method == 'PUT':
+        permission = get_object_or_404(Permissions, id=id)
+        serializer = Permissionserializer(permission, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
+    if request.method == 'POST':
+        serializer = Permissionserializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
