a95c324cd1a0fbe2e4fe64fa0d5109490503b72c
tal_l <tal9lev@gmail.com>
2024-08-20 12:07:32 +0300

    refactor: :art: ruff

--- todo_app_api/api/admin.py
+++ todo_app_api/api/admin.py
@@ -1,21 +1,24 @@
 from django.contrib import admin
 
-from .models import Group, Task, Permissions
+from .models import Group, Permissions, Task
+
 
 # Register your models here.
 @admin.register(Group)
 class GroupAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description')
-    search_fields = ('name',)
+    list_display = ("name", "description")
+    search_fields = ("name",)
+
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ('title', 'stage', 'group')
-    search_fields = ('title',)
-    list_filter = ('stage', 'group')
+    list_display = ("title", "stage", "group")
+    search_fields = ("title",)
+    list_filter = ("stage", "group")
+
 
 @admin.register(Permissions)
 class PermissionsAdmin(admin.ModelAdmin):
-    list_display = ('user', 'level', 'group')
-    search_fields = ('user',)
-    list_filter = ('level', 'group')
+    list_display = ("user", "level", "group")
+    search_fields = ("user",)
+    list_filter = ("level", "group")
--- todo_app_api/todo_app_api/settings.py
+++ todo_app_api/todo_app_api/settings.py
@@ -60,6 +60,8 @@ ACCOUNT_USERNAME_REQUIRED = False
 ACCOUNT_AUTHENTICATION_METHOD = "email"
 ACCOUNT_EMAIL_VERIFICATION = "optional"
 LOGIN_REDIRECT_URL = "/accounts"
+LOGOUT_REDIRECT_URL = "/accounts"
+
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
